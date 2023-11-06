e3e28a3362d325a6dfdbd4c3c79dd251ededac7e
imrun6am <125263494+imrun6am@users.noreply.github.com>
2023-11-06 17:55:11 +0600

    Fixed some magic method property related issue

--- app/Traits/FileManagerTrait.php
+++ app/Traits/FileManagerTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 
 trait FileManagerTrait
@@ -9,7 +10,7 @@ trait FileManagerTrait
     public static function upload(string $dir, string $format, $image = null): string
     {
         if ($image != null) {
-            $imageName = \Carbon\Carbon::now()->toDateString() . "-" . uniqid() . "." . $format;
+            $imageName = Carbon::now()->toDateString() . "-" . uniqid() . "." . $format;
             if (!Storage::disk('public')->exists($dir)) {
                 Storage::disk('public')->makeDirectory($dir);
             }
