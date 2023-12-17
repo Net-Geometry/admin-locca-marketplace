942f4ed8a9797a2415a074018b20b46d0c6b24b1
TawfiqRafi <rafi.6amtech@gmail.com>
2023-12-17 18:22:50 +0600

    vendor report,review, shop, wallet blade pages updated

---
+++ public/assets/admin/js/view-pages/vendor/wallet-method.js
@@ -0,0 +1,49 @@
+"use strict";
+
+$('#withdraw_method').on('change', function () {
+    $('#submit_button').attr("disabled","true");
+    let method_id = this.value;
+
+    // Set header if need any otherwise remove setup part
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $.ajax({
+        url: "{{route('vendor.wallet.method-list')}}" + "?method_id=" + method_id,
+        data: {},
+        processData: false,
+        contentType: false,
+        type: 'get',
+        success: function (response) {
+            $('#submit_button').removeAttr('disabled');
+            let method_fields = response.content.method_fields;
+            $("#method-filed__div").html("");
+            method_fields.forEach((element, index) => {
+                $("#method-filed__div").append(`
+                    <div class="form-group mt-2">
+                        <label for="wr_num" class="fz-16 c1 mb-2">${element.input_name.replaceAll('_', ' ')}</label>
+                        <input type="${element.input_type == 'phone' ? 'number' : element.input_type  }" class="form-control" name="${element.input_name}" placeholder="${element.placeholder}" ${element.is_required === 1 ? 'required' : ''}>
+                    </div>
+                `);
+            })
+
+        },
+        error: function () {
+
+        }
+    });
+});
+
+
+$('.showMyModal').on('click', function (){
+    let data = $(this).data('message');
+    showMyModal(data);
+})
+
+function showMyModal(data) {
+    $(".modal-body #hiddenValue").html(data);
+    $('#exampleModal').modal('show');
+}
+
