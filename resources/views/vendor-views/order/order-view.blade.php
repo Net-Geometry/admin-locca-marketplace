c16612ca6115dddb66fe3b7938ec220f8a7933da
nntuhin <tuhin@6amtech.net>
2023-12-21 17:17:26 +0600

    order amount edit updated

--- resources/views/vendor-views/order/order-view.blade.php
+++ resources/views/vendor-views/order/order-view.blade.php
@@ -1130,7 +1130,7 @@
                         <div class="form-group col-12">
                             <label for="order_amount">{{ translate('messages.order_amount') }}</label>
                             <input id="order_amount" type="number" class="form-control" name="order_amount" min="0"
-                                value="{{ $order['order_amount'] - $order['total_tax_amount']  - $order['additional_charge'] -  $order['delivery_charge'] + $order['store_discount_amount'] }}" step=".01">
+                                value="{{ abs($order['order_amount'] - $order['total_tax_amount']  - $order['additional_charge'] -  $order['delivery_charge'] + $order['store_discount_amount']) }}" step=".01">
                         </div>
 
                         <div class="form-group col-sm-12">
