2ca7d1bc4bc7c56bfdba827d51f2cae287ae4da6
TawfiqRafi <rafi.6amtech@gmail.com>
2023-12-24 11:41:01 +0600

    image issues fixing

--- public/assets/admin/js/view-pages/common.js
+++ public/assets/admin/js/view-pages/common.js
@@ -227,12 +227,12 @@ $(".set-filter").on("change", function () {
     tour.next();
 });
 
-$(document).ready(function() {
-    $('.onerror-image').on('error', function() {
-        let img = $(this).data('onerror-image')
-        $(this).attr('src', img);
-    });
-});
+// $(document).ready(function() {
+//     $('.onerror-image').on('error', function() {
+//         let img = $(this).data('onerror-image')
+//         $(this).attr('src', img);
+//     });
+// });
 
 $(document).on('click', '.confirm-Status-Toggle', function () {
     let Status_toggle = $('#toggle-status-ok-button').attr('toggle-ok-button');
--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -625,6 +625,16 @@
             })
         }
 
+document.addEventListener('DOMContentLoaded', function() {
+    let images = document.querySelectorAll('img');
+
+    images.forEach(function(image) {
+        let defaultImagePath = image.dataset.onerrorImage; // Use dataset to get the data attribute
+        image.addEventListener('error', function() {
+            this.src = defaultImagePath;
+        });
+    });
+});
 
 $(document).ready(function() {
     $('.onerror-image').each(function() {
