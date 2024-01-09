3ef7b1cf50e131bf6c89ba88f613a138e81cde07
TawfiqRafi <rafi.6amtech@gmail.com>
2024-01-09 11:46:32 +0600

    other banner issues fixed

--- resources/views/admin-views/other-banners/pharmacy-index.blade.php
+++ resources/views/admin-views/other-banners/pharmacy-index.blade.php
@@ -47,8 +47,8 @@
                                             <img class="onerror-image"
 
                                             src="{{ \App\CentralLogics\Helpers::onerror_image_helper(
-                                                $bottom_section_banner->value ?? '',
-                                                asset('storage/app/public/promotional_banner').'/'.$bottom_section_banner->value ?? '',
+                                                $bottom_section_banner?->value ?? '',
+                                                asset('storage/app/public/promotional_banner').'/'.$bottom_section_banner?->value ?? '',
                                                 asset('/public/assets/admin/img/upload-placeholder.png'),
                                                 'promotional_banner/'
                                             ) }}" data-onerror-image="{{asset('/public/assets/admin/img/upload-placeholder.png')}}" alt="">
