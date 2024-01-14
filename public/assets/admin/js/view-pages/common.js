c2b5986dccfd61d4f665eb119710d7b098944562
nntuhin <tuhin@6amtech.net>
2024-01-14 16:15:02 +0600

    mail template issue fixed

--- public/assets/admin/js/view-pages/common.js
+++ public/assets/admin/js/view-pages/common.js
@@ -116,7 +116,7 @@
                 $("#" + lang + "-form").removeClass('d-none');
                 $("#" + lang + "-form1").removeClass('d-none');
                 $("#" + lang + "-form2").removeClass('d-none');
-                $("#" +lang+" -form3").removeClass('d-none');
+                $("#" +lang+"-form3").removeClass('d-none');
                 $("#" +lang+"-form4").removeClass('d-none');
                 if (lang === 'default') {
                     $(".default-form").removeClass('d-none');
