1581a5d20f3165b8e1bae03804ced0baf2bca855
TawfiqRafi <rafi.6amtech@gmail.com>
2024-01-18 12:41:53 +0600

    store config model issue

--- app/Models/StoreConfig.php
+++ app/Models/StoreConfig.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class StoreConfig extends Model
 {
     use HasFactory;
-
+    protected $table;
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = $this->isTableNamedStoreConfigs() ? 'storeConfigs' : 'store_configs';
+    }
+    protected function isTableNamedStoreConfigs()
+    {
+        return $this->getConnection()->getDoctrineSchemaManager()->listTableNames() === ['storeConfigs'];
+    }
     protected $guarded = ['id'];
 
     protected $casts = [
