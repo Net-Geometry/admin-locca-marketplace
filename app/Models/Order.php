f9a463fe6848f67f1df50254566cda9c7d481e2d
TawfiqRafi <rafi.6amtech@gmail.com>
2024-06-02 13:26:04 +0600

    json image handle issue

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -55,7 +55,7 @@ class Order extends Model
         $value = is_array($this->order_attachment)?$this->order_attachment:json_decode($this->order_attachment,true);
         if ($value){
             foreach ($value as $item){
-                $item = is_array($item)?$item:(get_class($item) == 'stdClass' ? json_decode(json_encode($item), true):['img' => $item, 'storage' => 'public']);
+                $item = is_array($item)?$item:(is_object($item) && get_class($item) == 'stdClass' ? json_decode(json_encode($item), true):['img' => $item, 'storage' => 'public']);
                 if($item['storage']=='s3'){
                     $images[] = Helpers::s3_storage_link('order',$item['img']);
                 }else{
@@ -71,7 +71,7 @@ class Order extends Model
         $value = is_array($this->order_proof)?$this->order_proof:json_decode($this->order_proof,true);
         if ($value){
             foreach ($value as $item){
-                $item = is_array($item)?$item:(get_class($item) == 'stdClass' ? json_decode(json_encode($item), true):['img' => $item, 'storage' => 'public']);
+                $item = is_array($item)?$item:(is_object($item) && get_class($item) == 'stdClass' ? json_decode(json_encode($item), true):['img' => $item, 'storage' => 'public']);
                 if($item['storage']=='s3'){
                     $images[] = Helpers::s3_storage_link('order',$item['img']);
                 }else{
--- app/Models/TempProduct.php
+++ app/Models/TempProduct.php
@@ -61,7 +61,7 @@ class TempProduct extends Model
         $value = is_array($this->images)?$this->images:json_decode($this->images,true);
         if ($value){
             foreach ($value as $item){
-                $item = is_array($item)?$item:(get_class($item) == 'stdClass' ? json_decode(json_encode($item), true):['img' => $item, 'storage' => 'public']);
+                $item = is_array($item)?$item:(is_object($item) && get_class($item) == 'stdClass' ? json_decode(json_encode($item), true):['img' => $item, 'storage' => 'public']);
                 if($item['storage']=='s3'){
                     $images[] = Helpers::s3_storage_link('product',$item['img']);
                 }else{
