de16fabfd126e0d1a4dac98ee9a1bdf5670c789e
TawfiqRafi <rafi.6amtech@gmail.com>
2024-06-13 12:45:55 +0600

    deliveryman image issue

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4883,4 +4883,5 @@
   'Are you sure to disable the free trial option?' => 'Are you sure to disable the free trial option?',
   'If disabled, the store can’t get the experience without any business plan.' => 'If disabled, the store can’t get the experience without any business plan.',
   'You_have_received_new_message' => 'You have received new message',
+  'extra_packaging_amount_can_not_be_empty' => 'Extra packaging amount can not be empty',
 );
