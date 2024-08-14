945cf283fa23f551b67a580dee11b84e34366cca
nntuhin <tuhin@6amtech.net>
2024-08-14 12:44:45 +0600

    info added on firebase otp set up page

--- resources/views/admin-views/business-settings/firebase-otp-index.blade.php
+++ resources/views/admin-views/business-settings/firebase-otp-index.blade.php
@@ -16,7 +16,15 @@
                 </span>
             </h1>
             @include('admin-views.business-settings.partials.third-party-links')
-        </div>
+                <div class="py-1">
+                    <div class="text--primary-2 my-lg-n8 my-md-n8 mx-4 d-flex flex-wrap justify-content-end align-items-center" type="button" data-toggle="modal" data-target="#instructionsModal">
+                        <strong class="mr-2">{{translate('How it Works')}}</strong>
+                        <div class="blinkings">
+                            <i class="tio-info-outined"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         <!-- End Page Header -->
 
         <form
@@ -49,12 +57,12 @@
                                             <input type="checkbox"
                                                    data-id="firebase_otp_verification"
                                                    data-type="toggle"
-                                                   data-image-on="{{ asset('/public/assets/admin/img/modal/show-earning-in-apps-on.png') }}"
-                                                   data-image-off="{{ asset('/public/assets/admin/img/modal/show-earning-in-apps-off.png') }}"
-                                                   data-title-on="{{translate('Want_to_enable')}} <strong>{{translate('Cash_In_Hand_Overflow')}}</strong>"
-                                                   data-title-off="{{translate('Want_to_disable')}} <strong>{{translate('Cash_In_Hand_Overflow')}}</strong> "
-                                                   data-text-on="<p>{{ translate('If_enabled,_stores_have_to_provide_collected_cash_by_them_self') }}</p>"
-                                                   data-text-off="<p>{{ translate('If_disabled,_stores_do_not_have_to_provide_collected_cash_by_them_self') }}</p>"
+                                                   data-image-on="{{ asset('/public/assets/admin/img/modal/order-delivery-verification-on.png') }}"
+                                                   data-image-off="{{ asset('/public/assets/admin/img/modal/order-delivery-verification-off.png') }}"
+                                                   data-title-on="{{translate('Want_to_enable')}} <strong>{{translate('Firebase_OTP_Verification')}}</strong>"
+                                                   data-title-off="{{translate('Want_to_disable')}} <strong>{{translate('Firebase_OTP_Verification')}}</strong> "
+                                                   data-text-on="<p>{{ translate('If_enabled,Otp_will_sent_by_the_Firebase') }}</p>"
+                                                   data-text-off="<p>{{ translate('If_disabled,Otp_won’t_be_sent_by_the_Firebase') }}</p>"
                                                    class="status toggle-switch-input dynamic-checkbox-toggle"
                                                    value="1"
                                                    name="firebase_otp_verification" id="firebase_otp_verification"
@@ -74,11 +82,11 @@
                                                 {{ translate('Web_API_key') }}
                                             </span>
 
-                                            <span class="form-label-secondary"
+                                            {{-- <span class="form-label-secondary"
                                                   data-toggle="tooltip" data-placement="right"
                                                   data-original-title="{{ translate('Enter_the_maximum_cash_amount_stores_can_hold._If_this_number_exceeds,_stores_will_be_suspended_and_not_receive_any_orders.') }}"><img
                                                     src="{{ asset('/public/assets/admin/img/info-circle.svg') }}"
-                                                    alt="{{ translate('messages.dm_cancel_order_hint') }}"></span>
+                                                    alt="{{ translate('messages.dm_cancel_order_hint') }}"></span> --}}
                                         </label>
                                         <input type="text" name="firebase_web_api_key" class="form-control"
                                                id="firebase_web_api_key"
@@ -100,4 +108,34 @@
 
 
 
-@endsection
+
+    <div class="modal fade" id="instructionsModal" tabindex="-1" aria-labelledby="instructionsModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header d-flex justify-content-end">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="text-center my-5">
+                        <img src="{{ asset('public/assets/admin/img/modal/bell.png') }}">
+                    </div>
+
+                    <h5 class="modal-title my-3" id="instructionsModalLabel">{{translate('Instructions')}}</h5>
+                    <p>{{ translate('For configuring OTP in the Firebase, you must create a Firebase project first.If you haven’t created any project for your application yet, please create a project first.') }}
+                    </p>
+                    <p>{{ translate('Now go the') }} <a href="https://console.firebase.google.com/" target="_blank">Firebase console </a>{{ translate('and follow the instructions below') }} -</p>
+                    <ol class="d-flex flex-column __gap-5px __instructions">
+                        <li>{{ translate('Go to your Firebase project.') }}</li>
+                        <li>{{ translate('Navigate to the Build menu from the left sidebar and select Authentication.') }}</li>
+                        <li>{{ translate('Get started the project and go to the Sign-in method tab.') }}</li>
+                        <li>{{ translate('From the Sign-in providers section, select the Phone option.') }}</li>
+                        <li>{{ translate('Ensure to enable the method Phone and press save.') }}</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        </div>
+
+    @endsection
