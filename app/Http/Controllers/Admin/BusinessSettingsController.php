8f32fca196f06defd885acebbfc9e52d728dc8e6
TawfiqRafi <rafi.6amtech@gmail.com>
2024-09-12 16:32:53 +0600

    image velidation added in available zone setup

--- app/Http/Controllers/Admin/BusinessSettingsController.php
+++ app/Http/Controllers/Admin/BusinessSettingsController.php
@@ -3916,6 +3916,7 @@ class BusinessSettingsController extends Controller
                 $request->validate([
                     'available_zone_title.0' => 'required',
                     'available_zone_short_description.0' => 'required',
+                    'image' => 'required',
                 ], [
                     'available_zone_title.0.required' => translate('default_title_is_required'),
                     'available_zone_short_description.0.required' => translate('default_short_description_is_required'),
@@ -4501,6 +4502,7 @@ class BusinessSettingsController extends Controller
                 $request->validate([
                     'available_zone_title.0' => 'required',
                     'available_zone_short_description.0' => 'required',
+                    'image' => 'required',
                 ], [
                     'available_zone_title.0.required' => translate('default_title_is_required'),
                     'available_zone_short_description.0.required' => translate('default_short_description_is_required'),
@@ -5706,6 +5708,7 @@ class BusinessSettingsController extends Controller
                 $request->validate([
                     'available_zone_title.0' => 'required',
                     'available_zone_short_description.0' => 'required',
+                    'image' => 'required',
                 ], [
                     'available_zone_title.0.required' => translate('default_title_is_required'),
                     'available_zone_short_description.0.required' => translate('default_short_description_is_required'),
