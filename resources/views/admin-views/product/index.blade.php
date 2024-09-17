781f059f26c2ecd8dbee46ccecf6e80955b35fff
rafuj6amtech <aburaihan.6amtech@gmail.com>
2024-09-17 15:02:35 +0600

    Fix

--- resources/views/admin-views/product/index.blade.php
+++ resources/views/admin-views/product/index.blade.php
@@ -353,7 +353,7 @@
                                         </span> --}}
                                     </label>
                                     <div class="dropdown suggestion_dropdown">
-                                        <input type="text" class="form-control" data-toggle="dropdown" name="generic_name" autocomplete="off">
+                                        <input type="text" class="form-control" name="generic_name" autocomplete="off">
                                         @if(count(\App\Models\GenericName::select(['generic_name'])->get())>0)
                                         <div class="dropdown-menu">
                                             @foreach (\App\Models\GenericName::select(['generic_name'])->get() as $generic_name)
