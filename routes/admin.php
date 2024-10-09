d0b9b44da16fa0a23a50503e225b18011befaa26
neha <nehaa.6amtech@gmail.com>
2024-10-09 12:40:00 +0600

    modify: Admin-panel > test route add

--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Admin', 'as' => 'admin.'], function () {
 
     Route::group(['middleware' => ['admin', 'current-module']], function () {
+        Route::get('/test', function () {
+            return view('admin-views.login-setup.login_page');
+        });
+    
         Route::get('drivemond-panel', 'DriveMondController@drivemondExternalLogin')->name('drivemond-panel');
         Route::get('get-all-stores', 'VendorController@get_all_stores')->name('get_all_stores');
         Route::get('lang/{locale}', 'LanguageController@lang')->name('lang');
