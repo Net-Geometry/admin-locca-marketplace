53ee6a91c6944fb64cf40d737ea6e5f41c4a3102
neha <nehaa.6amtech@gmail.com>
2024-10-17 18:05:13 +0600

    Modify: Taxi module Dashboard chart data

--- resources/views/admin-views/car-rental/dashboard-car.blade.php
+++ resources/views/admin-views/car-rental/dashboard-car.blade.php
@@ -335,9 +335,9 @@
         // Static data for demonstration
         const hourlyCount = 450;
         const distancWiseCount = 50;
-        const totalSell = [200, 300, 400, 500, 600];
-        const commission = [20, 30, 40, 50, 60];
-        const deliveryCommission = [10, 15, 20, 25, 30];
+        const totalSell = [200, 300, 400, 500, 600,20, 30, 40, 50, 60, 100, 300];
+        const commission = [20, 30, 40, 50, 60,20, 30, 40, 50, 60, 100, 20];
+        const deliveryCommission = [10, 15, 20, 25, 30, 20, 30, 40, 50, 60, 100, 30];
         const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
         options = {
