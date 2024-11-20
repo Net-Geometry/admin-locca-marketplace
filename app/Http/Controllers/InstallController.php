2975adb9a6110ee15b4183ee60e8280646d6e316
TawfiqRafi <rafi.6amtech@gmail.com>
2024-11-20 15:51:09 +0600

    versionchange

--- app/Http/Controllers/InstallController.php
+++ app/Http/Controllers/InstallController.php
@@ -195,7 +195,7 @@ class InstallController extends Controller
                     BUYER_USERNAME=' . session('username') . '
                     SOFTWARE_ID=MzY3NzIxMTI=
 
-                    SOFTWARE_VERSION=2.11.1
+                    SOFTWARE_VERSION=2.12
                     REACT_APP_KEY=45370351
                     ';
             $file = fopen(base_path('.env'), 'w');
