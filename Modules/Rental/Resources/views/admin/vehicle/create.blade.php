027dc6047f1aec2129b1c84b5b7537022284f128
shakib6amtech <shakib.6amtech@gmail.com>
2025-01-14 14:58:20 +0600

    discount manage

--- Modules/Rental/Resources/views/admin/vehicle/create.blade.php
+++ Modules/Rental/Resources/views/admin/vehicle/create.blade.php
@@ -993,22 +993,36 @@
         });
 
         $(document).ready(function () {
+            function getApplicablePrice() {
+                let hourlyChecked = $('input[name="trip_hourly"]').is(':checked');
+                let distanceChecked = $('input[name="trip_distance"]').is(':checked');
+                let hourlyPrice = parseFloat($('input[name="hourly_price"]').val()) || 0;
+                let distancePrice = parseFloat($('input[name="distance_price"]').val()) || 0;
+
+                if (hourlyChecked && distanceChecked) {
+                    return Math.min(hourlyPrice, distancePrice);
+                } else if (hourlyChecked) {
+                    return hourlyPrice;
+                } else if (distanceChecked) {
+                    return distancePrice;
+                }
+                return 0;
+            }
+
             $('#discount_input').on('input', function () {
                 let discountType = $('#discount_type').val();
                 let inputValue = parseFloat($(this).val());
+                let applicablePrice = getApplicablePrice();
 
                 if (discountType === 'percent' && inputValue > 100) {
                     $(this).val(100);
+                } else if (discountType === 'amount' && inputValue > applicablePrice) {
+                    $(this).val(applicablePrice);
                 }
             });
 
-            $('#discount_type').on('change', function () {
-                let discountType = $(this).val();
-                let inputValue = parseFloat($('#discount_input').val());
-
-                if (discountType === 'percent' && inputValue > 100) {
-                    $('#discount_input').val(100);
-                }
+            $('input[name="trip_hourly"], input[name="trip_distance"], input[name="hourly_price"], input[name="distance_price"]').on('change input', function () {
+                $('#discount_input').trigger('input');
             });
         });
     </script>
