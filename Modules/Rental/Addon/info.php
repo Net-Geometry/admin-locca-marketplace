7605689b63d57365a88babd5c6c41f5e17f14b1a
TawfiqRafi <rafi.6amtech@gmail.com>
2025-01-23 17:33:22 +0600

    rental addon info updated

--- Modules/Rental/Addon/info.php
+++ Modules/Rental/Addon/info.php
@@ -1,5 +1,7 @@
 <?php return array(
     'software_id' => '48481246',
     'name' => 'Rental',
-    'is_published' => 0,
+    'is_published' => 1,
+    'purchase_code' => '342452',
+    'username' => 'derwf',
 );
