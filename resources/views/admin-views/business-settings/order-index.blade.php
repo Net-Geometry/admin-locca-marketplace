702dfb1c3905884245f278a075fdc569d8612b80
nntuhin <tuhin@6amtech.net>
2025-02-19 11:50:26 +0600

    ui remove for the cancellation rate limit

--- resources/views/admin-views/business-settings/order-index.blade.php
+++ resources/views/admin-views/business-settings/order-index.blade.php
@@ -270,7 +270,7 @@
                                     </div>
                                 </div>
 
-                                    @if (addon_published_status('Rental'))
+                                    {{-- @if (addon_published_status('Rental'))
                                     @php($order_cancelation_rate_limit_status = App\CentralLogics\Helpers::get_business_settings('order_cancelation_rate_limit_status') ?? 0)
 
                                     <div class="__bg-FAFAFA card shadow-none">
@@ -341,7 +341,7 @@
                                         </div>
                                     </div>
 
-                                    @endif
+                                    @endif --}}
 
 
 
