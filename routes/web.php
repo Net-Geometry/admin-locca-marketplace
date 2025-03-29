438f29090b0fd0cb9a13e9926377c325a122028a
NnTuhin <tuhin6amtech@gamil.com>
2025-03-29 15:06:53 +0600

    test commit

--- routes/web.php
+++ routes/web.php
@@ -214,4 +214,5 @@ Route::group(['prefix' => 'vendor', 'as' => 'restaurant.'], function () {
 Route::group(['prefix' => 'deliveryman', 'as' => 'deliveryman.'], function () {
     Route::get('apply', 'DeliveryManController@create')->name('create');
     Route::post('apply', 'DeliveryManController@store')->name('store');
+    // test
 });
