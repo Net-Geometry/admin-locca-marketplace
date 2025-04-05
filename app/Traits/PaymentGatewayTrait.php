ffbe2980256b5d92de796baa9584ba057a651153
nntuhin <tuhin@6amtech.net>
2025-04-05 13:46:49 +0600

    payment gateway supported currency file updated

--- app/Traits/PaymentGatewayTrait.php
+++ app/Traits/PaymentGatewayTrait.php
@@ -15,7 +15,11 @@ trait PaymentGatewayTrait
                 "JPY" => "Japanese Yen",
                 "AUD" => "Australian Dollar",
                 "NZD" => "New Zealand Dollar",
-                "CAD" => "Canadian Dollar"
+                "CAD" => "Canadian Dollar",
+                "DKK" => "Danish Krone",
+                "HKD" => "Hong Kong Dollar",
+                "SEK" => "Swedish Krona",
+                "CHF" => "Swiss Franc"
             ],
             "bkash" => [
                 "BDT" => "Bangladeshi Taka"
@@ -24,17 +28,27 @@ trait PaymentGatewayTrait
                 "INR" => "Indian Rupee"
             ],
             "ccavenue" => [
-                "INR" => "Indian Rupee"
-            ],
-            "ccavenue" => [
-                "INR" => "Indian Rupee"
+                "INR" => "Indian Rupee",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "AUD" => "Australian Dollar",
+                "CAD" => "Canadian Dollar",
+                "JPY" => "Japanese Yen",
+                "SGD" => "Singapore Dollar"
             ],
             "esewa" => [
                 "NPR" => "Nepalese Rupee"
             ],
             "fatoorah" => [
                 "KWD" => "Kuwaiti Dinar",
-                "SAR" => "Saudi Riyal"
+                "SAR" => "Saudi Riyal",
+                "AED" => "United Arab Emirates Dirham",
+                "BHD" => "Bahraini Dinar",
+                "QAR" => "Qatari Riyal",
+                "OMR" => "Omani Rial",
+                "EGP" => "Egyptian Pound",
+                "USD" => "United States Dollar"
             ],
             "flutterwave" => [
                 "NGN" => "Nigerian Naira",
@@ -43,10 +57,16 @@ trait PaymentGatewayTrait
                 "ZAR" => "South African Rand",
                 "USD" => "United States Dollar",
                 "EUR" => "Euro",
-                "GBP" => "Pound Sterling"
+                "GBP" => "Pound Sterling",
+                "RWF" => "Rwandan Franc",
+                "UGX" => "Ugandan Shilling",
+                "TZS" => "Tanzanian Shilling",
+                "ZMW" => "Zambian Kwacha"
             ],
             "foloosi" => [
-                "AED" => "United Arab Emirates Dirham"
+                "AED" => "United Arab Emirates Dirham",
+                "SAR" => "Saudi Riyal",
+                "USD" => "United States Dollar"
             ],
             "hubtel" => [
                 "GHS" => "Ghanaian Cedi"
@@ -59,21 +79,32 @@ trait PaymentGatewayTrait
                 "KWD" => "Kuwaiti Dinar",
                 "OMR" => "Omani Rial",
                 "QAR" => "Qatari Riyal",
-                "USD" => "United States Dollar"
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "JOD" => "Jordanian Dinar",
+                "LBP" => "Lebanese Pound"
             ],
             "instamojo" => [
                 "INR" => "Indian Rupee"
             ],
             "iyzi_pay" => [
-                "TRY" => "Turkish Lira"
+                "TRY" => "Turkish Lira",
+                "EUR" => "Euro",
+                "USD" => "United States Dollar",
+                "GBP" => "Pound Sterling"
             ],
             "liqpay" => [
                 "UAH" => "Ukrainian Hryvnia",
                 "USD" => "United States Dollar",
-                "EUR" => "Euro"
+                "EUR" => "Euro",
+                "RUB" => "Russian Ruble",
+                "BYN" => "Belarusian Ruble",
+                "KZT" => "Kazakhstani Tenge"
             ],
             "maxicash" => [
-                "PHP" => "Philippine Peso"
+                "PHP" => "Philippine Peso",
+                "USD" => "United States Dollar"
             ],
             "mercadopago" => [
                 "ARS" => "Argentine Peso",
@@ -83,19 +114,29 @@ trait PaymentGatewayTrait
                 "MXN" => "Mexican Peso",
                 "PEN" => "Peruvian Sol",
                 "UYU" => "Uruguayan Peso",
-                "USD" => "United States Dollar"
+                "USD" => "United States Dollar",
+                "PYG" => "Paraguayan Guarani",
+                "BOB" => "Bolivian Boliviano"
             ],
             "momo" => [
                 "VND" => "Vietnamese Dong"
             ],
             "moncash" => [
-                "HTG" => "Haitian Gourde"
+                "HTG" => "Haitian Gourde",
+                "USD" => "United States Dollar"
             ],
             "payfast" => [
-                "ZAR" => "South African Rand"
+                "ZAR" => "South African Rand",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling"
             ],
             "paymob_accept" => [
-                "EGP" => "Egyptian Pound"
+                "EGP" => "Egyptian Pound",
+                "AED" => "United Arab Emirates Dirham",
+                "SAR" => "Saudi Riyal",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro"
             ],
             "paypal" => [
                 "AUD" => "Australian Dollar",
@@ -123,14 +164,20 @@ trait PaymentGatewayTrait
                 "CHF" => "Swiss Franc",
                 "THB" => "Thai Baht",
                 "TRY" => "Turkish Lira",
-                "USD" => "United States Dollar"
+                "USD" => "United States Dollar",
+                "AED" => "United Arab Emirates Dirham",
+                "SAR" => "Saudi Riyal",
+                "ZAR" => "South African Rand"
             ],
             "paystack" => [
                 "NGN" => "Nigerian Naira",
                 "KES" => "Kenyan Shilling",
                 "GHS" => "Ghana Cedi",
                 "ZAR" => "South African Rand",
-                "XOF" => "West African CFA Franc countries",
+                "XOF" => "West African CFA Franc",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling"
             ],
             "paytabs" => [
                 "AED" => "United Arab Emirates Dirham",
@@ -140,7 +187,11 @@ trait PaymentGatewayTrait
                 "OMR" => "Omani Rial",
                 "QAR" => "Qatari Riyal",
                 "EGP" => "Egyptian Pound",
-                "USD" => "United States Dollar"
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "JOD" => "Jordanian Dinar",
+                "LBP" => "Lebanese Pound"
             ],
             "paytm" => [
                 "INR" => "Indian Rupee"
@@ -152,7 +203,15 @@ trait PaymentGatewayTrait
                 "NGN" => "Nigerian Naira"
             ],
             "razor_pay" => [
-                "INR" => "Indian Rupee"
+                "INR" => "Indian Rupee",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "SGD" => "Singapore Dollar",
+                "AED" => "United Arab Emirates Dirham",
+                "AUD" => "Australian Dollar",
+                "CAD" => "Canadian Dollar",
+                "JPY" => "Japanese Yen"
             ],
             "senang_pay" => [
                 "MYR" => "Malaysian Ringgit"
@@ -161,7 +220,12 @@ trait PaymentGatewayTrait
                 "BDT" => "Bangladeshi Taka"
             ],
             "ssl_commerz" => [
-                "BDT" => "Bangladeshi Taka"
+                "BDT" => "Bangladeshi Taka",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "CAD" => "Canadian Dollar",
+                "AUD" => "Australian Dollar"
             ],
             "stripe" => [
                 "USD" => "United States Dollar",
@@ -176,8 +240,16 @@ trait PaymentGatewayTrait
                 "NOK" => "Norwegian Krone",
                 "SEK" => "Swedish Krona",
                 "SGD" => "Singapore Dollar",
-                "RON" => "Romanian leu (RON) currency",
-                "HKD" => "Hong Kong Dollar"
+                "HKD" => "Hong Kong Dollar",
+                "RON" => "Romanian Leu",
+                "MXN" => "Mexican Peso",
+                "BRL" => "Brazilian Real",
+                "MYR" => "Malaysian Ringgit",
+                "PLN" => "Polish Zloty",
+                "THB" => "Thai Baht",
+                "CZK" => "Czech Koruna",
+                "HUF" => "Hungarian Forint",
+                "ILS" => "Israeli New Shekel"
             ],
             "swish" => [
                 "SEK" => "Swedish Krona"
@@ -188,19 +260,40 @@ trait PaymentGatewayTrait
                 "BHD" => "Bahraini Dinar",
                 "KWD" => "Kuwaiti Dinar",
                 "OMR" => "Omani Rial",
-                "QAR" => "Qatari Riyal"
+                "QAR" => "Qatari Riyal",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "JOD" => "Jordanian Dinar",
+                "EGP" => "Egyptian Pound"
             ],
             "thawani" => [
-                "OMR" => "Omani Rial"
+                "OMR" => "Omani Rial",
+                "AED" => "United Arab Emirates Dirham",
+                "USD" => "United States Dollar",
+                "EUR" => "Euro",
+                "SAR" => "Saudi Riyal",
+                "QAR" => "Qatari Riyal",
+                "BHD" => "Bahraini Dinar",
+                "KWD" => "Kuwaiti Dinar"
             ],
             "viva_wallet" => [
-                "EUR" => "Euro"
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "BGN" => "Bulgarian Lev",
+                "RON" => "Romanian Leu",
+                "PLN" => "Polish Zloty"
             ],
             "worldpay" => [
                 "GBP" => "Pound Sterling",
                 "USD" => "United States Dollar",
                 "EUR" => "Euro",
-                "JPY" => "Japanese Yen"
+                "JPY" => "Japanese Yen",
+                "CAD" => "Canadian Dollar",
+                "AUD" => "Australian Dollar",
+                "NZD" => "New Zealand Dollar",
+                "HKD" => "Hong Kong Dollar",
+                "SGD" => "Singapore Dollar"
             ],
             "xendit" => [
                 "IDR" => "Indonesian Rupiah",
@@ -208,8 +301,37 @@ trait PaymentGatewayTrait
                 "VND" => "Vietnamese Dong",
                 "THB" => "Thai Baht",
                 "MYR" => "Malaysian Ringgit",
-                "SGD" => "Singapore Dollar"
+                "SGD" => "Singapore Dollar",
+                "USD" => "United States Dollar"
             ],
+            "dlocal" => [
+                "ARS" => "Argentine Peso",
+                "BRL" => "Brazilian Real",
+                "CLP" => "Chilean Peso",
+                "COP" => "Colombian Peso",
+                "MXN" => "Mexican Peso",
+                "PEN" => "Peruvian Sol",
+                "UYU" => "Uruguayan Peso",
+                "EGP" => "Egyptian Pound",
+                "INR" => "Indian Rupee",
+                "NGN" => "Nigerian Naira",
+                "ZAR" => "South African Rand"
+            ],
+            "mollie" => [
+                "EUR" => "Euro",
+                "GBP" => "Pound Sterling",
+                "USD" => "United States Dollar",
+                "CAD" => "Canadian Dollar",
+                "AUD" => "Australian Dollar",
+                "NZD" => "New Zealand Dollar",
+                "CHF" => "Swiss Franc",
+                "DKK" => "Danish Krone",
+                "NOK" => "Norwegian Krone",
+                "SEK" => "Swedish Krona",
+                "PLN" => "Polish Zloty",
+                "CZK" => "Czech Koruna",
+                "HUF" => "Hungarian Forint"
+            ]
         ];
 
         if ($key) {
