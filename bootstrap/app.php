ec1547ae8b13a9f6caddd229ce4fe3bcb6d51fb7
TawfiqRafi <rafi.6amtech@gmail.com>
2025-05-14 12:22:40 +0600

    image proxy route added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,7 +51,10 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-//header('Access-Control-Allow-Origin: *');
+// $requestUri = $_SERVER['REQUEST_URI'] ?? '';
+// if (!str_starts_with($requestUri, '/image-proxy')){
+//     header('Access-Control-Allow-Origin: *');
+// }
 //header('Access-Control-Allow-Methods: *');
 //header('Access-Control-Allow-Headers: *');
 
