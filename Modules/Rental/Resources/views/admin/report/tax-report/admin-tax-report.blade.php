6807c556606193fa3c45fe827066f055f13deb55
NnTuhin <tuhin.6amtech@gmail.com>
2025-06-26 16:16:49 +0600

    js moved to file

--- Modules/Rental/Resources/views/admin/report/tax-report/admin-tax-report.blade.php
+++ Modules/Rental/Resources/views/admin/report/tax-report/admin-tax-report.blade.php
@@ -284,44 +284,9 @@
 @endsection
 
 @push('script_2')
+    <script src="{{asset('Modules/Rental/public/assets/js/admin/view-pages/admin-tax-report.js')}}"></script>
     <script>
         "use strict";
-
-        $(document).on('ready', function() {
-            function updateUI() {
-                if ($('#date_range_type').val() == 'custom') {
-                    $('#date_range').removeClass('d-none');
-                } else {
-                    $('#date_range').addClass('d-none');
-                }
-
-                if ($('#calculate_tax_on').val() == 'individual_source') {
-                    $('#calculate_commission_tax').removeClass('d-none');
-                    $('#calculate_delivery_charge_tax').removeClass('d-none');
-                    $('#calculate_service_charge_tax').removeClass('d-none');
-                    // $('#calculate_packaging_charge_tax').removeClass('d-none');
-                    $('#calculate_subscription_tax').removeClass('d-none');
-                    $('#calculate_tax_rate').addClass('d-none').find('select').attr('required', false);
-                } else {
-                    $('#calculate_tax_rate').removeClass('d-none').find('select').attr('required', true);
-                    $('#calculate_commission_tax').addClass('d-none');
-                    $('#calculate_delivery_charge_tax').addClass('d-none');
-                    $('#calculate_service_charge_tax').addClass('d-none');
-                    // $('#calculate_packaging_charge_tax').addClass('d-none');
-                    $('#calculate_subscription_tax').addClass('d-none');
-                }
-            }
-            updateUI();
-            $('#date_range_type').on('change', updateUI);
-            $('#calculate_tax_on').on('change', updateUI);
-            $('#reset_button_id').on('click', function() {
-                $('.js-select2-custom').val(null).trigger('change');
-                setTimeout(() => {
-                    updateUI();
-                }, 1);
-            });
-        });
-
 
         $(function() {
             $('input[name="dates"]').daterangepicker({
