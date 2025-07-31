efcf62d8108e275541fc6251e360e6ffc68083bf
mustafizur6amtech <mostafiz.6amtech@gmail.com>
2025-07-31 09:47:05 +0600

    fixed toastrs issue

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -17,7 +17,7 @@ use Illuminate\Http\JsonResponse;
 use App\Models\SubscriptionPackage;
 use Gregwar\Captcha\CaptchaBuilder;
 use App\Mail\VendorSelfRegistration;
-use Brian\Toastr\Facades\Toastr;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
